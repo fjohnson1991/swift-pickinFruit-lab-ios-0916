a0cd1a295f7f0716ca98a1c1ca5612ebc0dd5c9c
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-10-20 09:56:32 -0400

    commit

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var fruitPicker: UIPickerView!
+    
+    
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
     
@@ -19,15 +22,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        fruitPicker.dataSource = self
+        fruitPicker.delegate = self
         
     }
     
     @IBAction func spinButtonTapped(_ sender: UIButton) {
+        for i in 0..<3 {
+            let row = arc4random_uniform(UInt32(fruitsArray.count))
+            fruitPicker.selectRow(Int(row), inComponent: i, animated: true)
+        }
         
+        let fruitOne = fruitsArray[fruitPicker.selectedRow(inComponent: 0)]
+        let fruitTow = fruitsArray[fruitPicker.selectedRow(inComponent: 1)]
+        let fruitThree = fruitsArray[fruitPicker.selectedRow(inComponent: 2)]
         
-        
+        if fruitOne == fruitTow && fruitTow == fruitThree {
+            resultLabel.text = "WINNER!"
+        } else {
+            resultLabel.text = "TRY AGAIN"
+        }
     }
-
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruitsArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruitsArray[row]
+    }
 }
 
 // MARK: Set Up
